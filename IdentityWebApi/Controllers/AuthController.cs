9ed201914439d2fc2ad271cdb145a1884235bcdd
M Faisal Hayat <m.faisalhayat3@gmail.com>
2023-08-10 12:54:31 +0500

    controller for authentication has been added

---
+++ IdentityWebApi/Controllers/AuthController.cs
@@ -0,0 +1,34 @@
+﻿using IdentityWebApi.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private UserManager<IdentityUser> _userManager;
+        private RoleManager<IdentityRole> _roleManager;
+        private IConfiguration _configuration;
+
+
+        public AuthController(ApplicationDbContext context, RoleManager<IdentityRole> roleManager, IConfiguration configuration,
+            UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _roleManager = roleManager;
+            _configuration = configuration;
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        [Route("Authentication")]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
--- IdentityWebApi/Data/ApplicationDbContext.cs
+++ IdentityWebApi/Data/ApplicationDbContext.cs
@@ -22,7 +22,7 @@ namespace IdentityWebApi.Data
         private static void SeedRoles(ModelBuilder builder)
         {
             builder.Entity<IdentityRole>().HasData(
-                new IdentityRole() { Name="Admin", ConcurrencyStamp="1", NormalizedName="Admin"},
+                new IdentityRole() { Name= "Admin", ConcurrencyStamp="1", NormalizedName="Admin"},
                 new IdentityRole() { Name = "User", ConcurrencyStamp = "2", NormalizedName = "User" },
                 new IdentityRole() { Name = "HR", ConcurrencyStamp = "3", NormalizedName = "HR" }
                 );
